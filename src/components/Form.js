2642971f1c3ff2d52613c21a51b2187d1723e57e
linas19 <linas.jonika1@gmail.com>
2021-07-01 17:18:08 +0300

    final

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,27 +3,27 @@ import React from "react";
 export default function Form() {
     return (
         <div id="form">
-          <h1 >
-            <span>Contact</span>
-          </h1>
-            <form name="contact" method="post">
-                <input type="hidden" name="form-name" value="contact" />
-                <p>
-                    <label htmlFor="name">Name</label> <br />
-                    <input type="text" id="name" name="name" required />
-                </p>
-                <p>
-                    <label htmlFor="email">Email</label> <br />
-                    <input type="email" id="email" name="email" required />
-                </p>
-                <p>
-                    <label htmlFor="message">Message</label> <br />
-                    <textarea id="message" name="message" required></textarea>
-                </p>
-                <p>
-                    <input type="submit" value="Submit message" />
-                </p>
-            </form>
+            <h1 >
+                <span>Contact</span>
+            </h1>
+            <div className="form-fields">
+                <form name="contact" method="post">
+                    <input type="hidden" name="form-name" value="contact" />
+                    <p>
+                        <input className="input-field" type="text" id="name" name="name" placeholder="Name" required />
+                    </p>
+                    <p>
+                        <input className="input-field" type="email" id="email" name="email" placeholder="Email" required />
+                    </p>
+                    <p>
+                        <textarea id="message" name="message" placeholder="Message" required></textarea>
+                    </p>
+                    <p>
+                        <input id="submit" type="submit" value="Submit message" />
+                    </p>
+                </form>
+            </div>
+
         </div>
 
     );
