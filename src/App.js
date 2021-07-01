9586f093f8573e9ae66e2ab7d8d4f9b560dba847
linas19 <linas.jonika1@gmail.com>
2021-07-01 14:58:09 +0300

    changed form code

--- src/App.js
+++ src/App.js
@@ -168,7 +168,27 @@ class App extends Component {
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
         <div ref={this.getInTouchRef}></div>
-        <FormSection />
+        {/* <FormSection /> */}
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Your Role: <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
       </div>
     );
